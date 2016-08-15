df4cdbe9f6e3c5375f6b1e255965ca3b2a347175
lu.chen <562890847@qq.com>
2016-08-15 21:39:04 +0800

    update:error

---
+++ app/server/service/articleSer.js
@@ -0,0 +1,17 @@
+
+/**
+ * Created by ygy on 2016/8/13.
+ */
+var Article = require("../model/articleMod.js");
+
+var article = new Article();
+article.title = 'article';
+article.content = 'content';
+article.category = 'article';
+article.title = 'article';
+article.title = 'article';
+article.title = 'article';
+article.title = 'article';
+article.title = 'article';
+article.title = 'article';
+
